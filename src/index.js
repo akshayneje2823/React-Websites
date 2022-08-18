76884692fb2634c171f49a25a31360489de9f76f
Akshay Neje <akshayneje2823@gmail.com>
2022-08-18 17:31:34 +0530

    DONE

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Registrstion/App';
+import App from './pagination/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ src/pagination/App.jsx
@@ -0,0 +1,12 @@
+import React,{useEffect ,useState} from 'react';
+import  './App.css'
+
+function App() {
+    const [, set] = useState(second)
+    
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
