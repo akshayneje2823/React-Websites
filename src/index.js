db180dbaf3b8e360c6081c3dd6061ae383ad9495
Akshay Neje <akshayneje2823@gmail.com>
2022-08-19 12:04:59 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pagination/App';
+import App from './project-setUp/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
